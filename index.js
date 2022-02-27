9dbdc391b0179a9f087751b9cc1fcb0b7a20bdad
Michelle Blackwell <92054570+mblackwellgca@users.noreply.github.com>
2022-02-27 15:13:08 -0500

    added prompts to index.js

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
+    return inquirer.prompt([
+        {
+        type: 'input',
+        name: 'title',
+        message: 'What is the project title?',
+        },
+        {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description of your project?',
+        },
+        {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+        },
+        {
+        type: 'input',
+        name: 'usage',
+        message: 'Examples and instructions for use',
+        },
+        {
+        type: 'input',
+        name: 'credits',
+        message: 'Porject collaborators',
+        },
+        {
+        type: 'input',
+        name: 'license',
+        message: 'Include the license.',
+        },
+    ]);
+    };
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
